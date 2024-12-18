cd1439b3a911a286947ca2df017eb14d8d3286b0
Denis Belov <dsbelov63@gmail.com>
2024-12-18 12:33:45 +0400

    auto

---
+++ 0.worker.worker.js
@@ -0,0 +1,21 @@
+self["webpackChunk"]([0],{
+
+/***/ "../../renderity/wasm-wrapper/src/test-atomic/src/test-atomic.worker.js":
+/*!******************************************************************************************!*\
+  !*** /Users/Denis/reps/renderity/wasm-wrapper/src/test-atomic/src/test-atomic.worker.js ***!
+  \******************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Worker_fn; });
+function Worker_fn() {
+  return new Worker(__webpack_require__.p + "test-atomic.worker.worker.worker.worker.js");
+}
+
+
+/***/ })
+
+});
+//# sourceMappingURL=0.worker.worker.js.map
